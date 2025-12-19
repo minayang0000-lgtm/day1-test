b8c035529ebb1becafc46769387af97c66467eae
Mina Yang <minayang0000@gmail.com>
2025-12-19 02:01:38 -0500

    variablels

---
+++ Variables.py
@@ -0,0 +1,29 @@
+# first git
+# strings (are in quotes)
+print ("Stinky Feet")
+color = "purple"
+object = "chair"
+email = "simondogeater123@chinese.com"
+print (f"My favorite item is a {color} {object}")
+print (f"I dislike {color} {object}")
+
+# Integers (are whole numbers)
+age = 18
+quantity = 734
+
+print (f"You are {age} years old and you have {quantity} wrinkles on your face already gahdamn")
+
+# Floats (contain a decimal point)
+height = 5.9
+weight = 165.5
+gpa = 3.67
+gpa_2 = 4.0
+print (f"You are {height} feet tall and you weigh {weight} pounds, you are a fatty")
+print (f"My gpa is {gpa} but it needs to be a {gpa_2} to make me happy")
+
+# Booleans (are true or false)
+is_tall = True
+is_rich = False
+is_happy = True
+print (f"You are tall: {is_tall} and you are rich: {is_rich}")
+print (f"You are happy: {is_happy}")
--- simon.py
+++
@@ -1,6 +0,0 @@
-# first git
-print ("stinky feet")
-color = "purple"
-object = "chair"
-print (f"My favorite item is a {color} {object}")
-print (f"I dislike {color} {object}")
